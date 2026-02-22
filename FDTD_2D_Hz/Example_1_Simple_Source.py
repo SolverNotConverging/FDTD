07baa831c324fe68df3027f2873e6409b776e70b
SolverNotConverging <mostima0@outlook.com>
2026-02-22 09:23:32 +0000

    Update GPU support for Apple Silicon

--- FDTD_2D_Hz/Example_1_Simple_Source.py
+++ FDTD_2D_Hz/Example_1_Simple_Source.py
@@ -1,6 +1,6 @@
 from FDTD_2D_Hz_GPU import FDTD_2D_Hz_GPU
 
-sim = FDTD_2D_Hz_GPU(x_range=14e-3, y_range=14e-3, Nx=140, Ny=140, f_min=50e9, f_max=100e9, Nt=1000)
+sim = FDTD_2D_Hz_GPU(x_range=14e-3, y_range=14e-3, Nx=140, Ny=140, f_min=50e9, f_max=100e9, Nt=4000)
 sim.periodic = ''
 sim.add_PML(pml_width=20, order=3, direction='xy', kappa_max=7, alpha_max=0.025)
 
@@ -17,7 +17,7 @@ sim.run(record_stride=1)
 sim.save("fdtd_run.pkl", include_histories=True)
 
 power_results = sim.calculate_line_monitor_power_fft(monitor_index=0, window=None)
-source_results = sim.calculate_source_power_fft(source_index=0)
+source_results = sim.calculate_source_power_fft(source_index=1)
 sim.plot_fft_results((power_results, source_results))
 
 # Dynamic color scaling in the animation (smoothed to reduce flicker)
